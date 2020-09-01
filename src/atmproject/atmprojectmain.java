630536b07ff564f898305c49df4c2779e10623f8
malaikakhalid <70518776+malaikakhalid@users.noreply.github.com>
2020-09-01 23:49:33 +0500

    Add files via upload

---
+++ src/atmproject/atmprojectmain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atmproject;
+
+/**
+ *
+ * @author HP
+ */
+public class atmprojectmain {
+    
+    public static void main(String[] args) {
+        
+        new login_Form().setVisible(true);
+        
+    }
+    
+}
